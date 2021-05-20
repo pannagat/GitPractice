2a2fa4530a9d5c8f3123dcea4146a219cc4de1af
pannaga32@gmail.com <pannaga32@gmail.com>
2021-05-20 18:12:14 +0530

    adding lengthstring without length method

---
+++ lengthstring2.java
@@ -0,0 +1,13 @@
+class lengthstring2
+{
+public static void main(String args[])
+{
+String s="I hate automation testing";
+int i=0;
+for(char c:s.toCharArray())
+{
+i++;
+}
+System.out.println("Length of a string " +i);
+}
+}
